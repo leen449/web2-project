a6c61fd22a6e468922621e80d46f86b069d9cf61
leen449 <leenmeakel@gmail.com>
2025-10-03 16:29:21 +0300

    testing

--- web2/Educators homepage.php
+++ web2/Educators homepage.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Educator's Homepage</title>
+    <title>Educators Homepage</title>
     <link rel="stylesheet" href="Educator's homepage.css" />
     <link rel="stylesheet" href="style.css" />
   </head>
